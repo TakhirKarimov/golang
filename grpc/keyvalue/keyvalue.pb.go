200a158c8decb3e8030c87386502485b5e961045
Takhir <80198444+TakhirKarimov@users.noreply.github.com>
2023-08-08 09:23:42 +0300

    Update keyvalue.pb.go

--- grpc/keyvalue/keyvalue.pb.go
+++ grpc/keyvalue/keyvalue.pb.go
@@ -4,7 +4,7 @@
 // 	protoc        v4.23.4
 // source: keyvalue.proto
 
-package __
+package github.com/TakhirKarimov/golang/tree/main/grpc/keyvalue
 
 import (
 	protoreflect "google.golang.org/protobuf/reflect/protoreflect"
